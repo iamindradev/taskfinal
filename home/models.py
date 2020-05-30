5fcff389e43579d72b9b97cc1a00b775db41d336
indradev <yadavdevendra99876@gmail.com>
2020-05-30 23:20:33 +0530

    after upgradation

--- home/models.py
+++ home/models.py
@@ -1,32 +1,25 @@
 from django.db import models
 
 # Create your models here.
-class login(models.Model):
-    link_teacher = models.ForeignKey('teacher' default='SOME STRING',on_delete=models.CASCADE)
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=100)
-    def __str__(self):
-        return self.username
-    
 
-class student(models.Model):
-    lib_id = models.CharField(max_length=50)
-    name = models.CharField(max_length=100)
-    branch = models.CharField(max_length=50)
-    section=models.CharField(max_length=2)
-    def __str__(self):
-        return self.name
-    
+
+
 class mark(models.Model):
     maths = models.IntegerField()
     chemisty= models.IntegerField()
     c_programming=models.IntegerField()
     english=models.IntegerField()
     
-    
+class student(models.Model):
+    name = models.CharField(max_length=100)
+    branch = models.CharField(max_length=50)
+    section=models.CharField(max_length=2)
+    def __str__(self):
+        return self.name
+        
     
 class teacher(models.Model):
-    lib_id=models.CharField(max_length=50)
+
     name = models.CharField(max_length=100)
     father_name= models.CharField(max_length=200)
     email=models.EmailField( max_length=254)
@@ -38,6 +31,11 @@ class teacher(models.Model):
     def __str__(self):
         return self.name
 
-
+class login(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=100)
+    def __str__(self):
+        return self.username
+    
 
 
